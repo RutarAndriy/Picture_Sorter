82616a194ce02639a63dcf64cfceee58c5f4b941
Rutar Andriy <RutarAndriy@gmail.com>
2024-01-12 15:04:26 +0200

    Generated project structure

---
+++ src/test/java/com/rutar/picture_sorter/AppTest.java
@@ -0,0 +1,19 @@
+package com.rutar.picture_sorter;
+
+import org.junit.*;
+
+import static org.junit.Assert.*;
+
+// ............................................................................
+
+public class AppTest {
+
+///////////////////////////////////////////////////////////////////////////////
+
+@Test
+public void should_Answer_With_True()
+    { assertTrue(true); }
+
+///////////////////////////////////////////////////////////////////////////////
+
+}
